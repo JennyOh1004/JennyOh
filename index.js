a040029890247bbc261a0ad19dea4ada7e8d9907
soobing <qls0147@naver.com>
2020-05-24 20:16:03 +0900

    feat: animation 추가

--- index.js
+++ index.js
@@ -9,6 +9,34 @@ window.onmousemove = function (e) {
   circle.style.left = newx + 'px';
 };
 
+
+function isElementInViewport(el) {
+
+  // Special bonus for those using jQuery
+  if (typeof jQuery === "function" && el instanceof jQuery) {
+    el = el[0];
+  }
+
+  var rect = el.getBoundingClientRect();
+
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && /* or $(window).height() */
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth) /* or $(window).width() */
+  );
+}
+const aboutEl = document.getElementById('about__title');
 window.addEventListener("scroll", (e) => {
-  console.log(window.scrollY);
+  // console.log(window.scrollY);
+  const isAboutElInView = isElementInViewport(aboutEl);
+
+  if (isAboutElInView) {
+    console.log('in view');
+    if (!aboutEl.classList.contains('slide-up')) {
+      aboutEl.classList.add("slide-up");
+    }
+  } else {
+    aboutEl.classList.remove("slide-up");
+  }
 });
