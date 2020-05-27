36a33292e6556a575753fbfc8828c2b5878ed028
JennyOh1004 <ohseulki1004@gmail.com>
2020-05-26 19:06:11 -0700

    Add chart.js file

---
+++ chart.js
@@ -0,0 +1,170 @@
+//flat skill chart
+
+// Hide Legend
+Chart.defaults.global.legend.display = false;
+// Create Chart
+var riskMeterctx = document.getElementById("myHtml");
+var myChart = new Chart(riskMeterctx, {
+  type: "doughnut",
+  data: {
+    labels: ["HTML"],
+    datasets: [
+      {
+        data: [80, 20],
+        backgroundColor: ["#E57373"],
+        hoverBackgroundColor: ["#36A2EB"],
+        borderWidth: 0,
+      },
+    ],
+  },
+  options: {
+    animation: {
+      animateScale: true,
+      cutoutPercentage: 80,
+    },
+    title: {
+      display: true,
+      text: "HTML",
+    },
+  },
+});
+
+// Hide Legend
+Chart.defaults.global.legend.display = false;
+// Create Chart
+var riskMeterctx = document.getElementById("myCss");
+var myChart = new Chart(riskMeterctx, {
+  type: "doughnut",
+  data: {
+    labels: ["CSS"],
+    datasets: [
+      {
+        data: [80, 20],
+        backgroundColor: ["#FF8A65"],
+        hoverBackgroundColor: ["#36A2EB"],
+        borderWidth: 0,
+      },
+    ],
+  },
+  options: {
+    animation: {
+      animateScale: true,
+    },
+    title: {
+      display: true,
+      text: "CSS",
+    },
+  },
+});
+
+// Hide Legend
+Chart.defaults.global.legend.display = false;
+// Create Chart
+var riskMeterctx = document.getElementById("myJs");
+var myChart = new Chart(riskMeterctx, {
+  type: "doughnut",
+  data: {
+    labels: ["JS"],
+    datasets: [
+      {
+        data: [70, 30],
+        backgroundColor: ["#FFF176"],
+        hoverBackgroundColor: ["#36A2EB"],
+        borderWidth: 0,
+      },
+    ],
+  },
+  options: {
+    animation: {
+      animateScale: true,
+    },
+    title: {
+      display: true,
+      text: "JavaScript",
+    },
+  },
+});
+
+// Hide Legend
+Chart.defaults.global.legend.display = false;
+// Create Chart
+var riskMeterctx = document.getElementById("myReact");
+var myChart = new Chart(riskMeterctx, {
+  type: "doughnut",
+  data: {
+    labels: ["React"],
+    datasets: [
+      {
+        data: [60, 40],
+        backgroundColor: ["#81C784"],
+        hoverBackgroundColor: ["#36A2EB"],
+        borderWidth: 0,
+      },
+    ],
+  },
+  options: {
+    animation: {
+      animateScale: true,
+    },
+    title: {
+      display: true,
+      text: "React",
+    },
+  },
+});
+
+// Hide Legend
+Chart.defaults.global.legend.display = false;
+// Create Chart
+var riskMeterctx = document.getElementById("myRuby");
+var myChart = new Chart(riskMeterctx, {
+  type: "doughnut",
+  data: {
+    labels: ["Ruby"],
+    datasets: [
+      {
+        data: [60, 40],
+        backgroundColor: ["#64B5F6"],
+        hoverBackgroundColor: ["#36A2EB"],
+        borderWidth: 0,
+      },
+    ],
+  },
+  options: {
+    animation: {
+      animateScale: true,
+    },
+    title: {
+      display: true,
+      text: "Ruby",
+    },
+  },
+});
+
+// Hide Legend
+Chart.defaults.global.legend.display = false;
+// Create Chart
+var riskMeterctx = document.getElementById("myRails");
+var myChart = new Chart(riskMeterctx, {
+  type: "doughnut",
+  data: {
+    labels: ["Rails"],
+    datasets: [
+      {
+        data: [70, 30],
+        backgroundColor: ["#BA68C8"],
+        hoverBackgroundColor: ["#36A2EB"],
+        borderWidth: 0,
+      },
+    ],
+  },
+  options: {
+    animation: {
+      animateScale: true,
+    },
+    title: {
+      display: true,
+      text: "Rails",
+    },
+  },
+});
