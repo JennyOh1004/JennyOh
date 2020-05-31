586451681510679884f8376576bf1ec44e1c1248
JennyOh1004 <ohseulki1004@gmail.com>
2020-05-30 17:03:05 -0700

    Update css file

---
+++ nav.js
@@ -0,0 +1,21 @@
+// nav effect
+
+// const sections = document.querySelectorAll("section");
+// const bubble = document.querySelector(".bubble");
+
+// const options = {
+//   threshold: 0.7,
+// };
+
+// let observer = new IntersectionObserver(navCheck, options);
+
+// function navCheck(entries) {
+//   entries.forEach((entry) => {
+//     // console.log(entry);
+//     const className = entry.target.className;
+//     console.log(className);
+//   });
+// }
+// sections.forEach((section) => {
+//   observer.observe(section);
+// });
